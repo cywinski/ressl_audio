correct the normalization

dataset/audio_data.py
@@ -12,14 +12,12 @@ class BaseRawAudioDataset(torch.utils.data.Dataset):
         random_crop=False,
         contrastive_aug=None,
         weak_aug=None,
-        pre_norm=None,
     ):
         self.sample_rate = sample_rate
         self.unit_samples = int(sample_rate * 0.95)
         self.random_crop = random_crop
         self.contrastive_aug = contrastive_aug
         self.weak_aug = weak_aug
-        self.pre_norm = pre_norm
 
     def __len__(self):
         raise NotImplementedError("implement me")
@@ -53,11 +51,7 @@ class BaseRawAudioDataset(torch.utils.data.Dataset):
             pos_1 = torch.from_numpy(wav).float()
             pos_2 = torch.from_numpy(wav).float()
 
-        if self.pre_norm:
-            pos_1 = self.pre_norm(pos_1)
-            pos_2 = self.pre_norm(pos_2)
-
-        # Return item
+        # Return pair of waveforms
         label = self.get_label(index)
         return (pos_1, pos_2) if label is None else (pos_1, label, pos_2, label)
 
@@ -71,14 +65,12 @@ class WavDatasetPair(BaseRawAudioDataset):
         random_crop=False,
         contrastive_aug=None,
         weak_aug=None,
-        pre_norm=None,
     ):
         super().__init__(
             sample_rate=sample_rate,
             random_crop=random_crop,
             contrastive_aug=contrastive_aug,
             weak_aug=weak_aug,
-            pre_norm=pre_norm,
         )
         self.files = audio_files
         self.labels = labels

ressl.py
@@ -69,7 +69,7 @@ to_spec = nnAudio.features.MelSpectrogram(
 
 
 def calc_norm_stats(data_loader, n_stats=10000, device="cuda"):
-    # Calculate normalization statistics from the training dataset.
+    # Calculate normalization statistics from the training dataset (spectrograms).
     n_stats = min(n_stats, len(data_loader.dataset))
     print(
         f"Calculating mean/std using random {n_stats} samples from population {len(data_loader.dataset)} samples..."
@@ -117,6 +117,10 @@ def train(
         # Raw audio to log-mel spectrograms
         img1 = (to_spec(wav1) + torch.finfo().eps).log().unsqueeze(1)
         img2 = (to_spec(wav2) + torch.finfo().eps).log().unsqueeze(1)
+
+        if pre_norm is not None:
+            img1 = pre_norm(img1)
+            img2 = pre_norm(img2)
 
         img1 = img1.cuda(non_blocking=True)
         img2 = img2.cuda(non_blocking=True)
@@ -197,7 +201,6 @@ def main():
         random_crop=True,
         contrastive_aug=get_contrastive_augment(),
         weak_aug=get_weak_augment(),
-        pre_norm=pre_norm,
     )
     train_loader = DataLoader(
         dataset,